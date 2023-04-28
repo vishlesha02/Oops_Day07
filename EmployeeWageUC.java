bedae9fb7641e8e4181c21a85864a8406a52ad28
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-28 13:32:21 +0530

    Add files via upload

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -9,13 +9,18 @@ public class EmployeeWageUC {
     public static final int IS_FULL_TIME = 2;
     public static final int Emp_RATE_PER_HR = 20;
     public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_HR_IN_MONTH=100;
 
     public static void main(String[] args) {
         int EmpHr = 0;
+        int day=1;
+        int Total_Emp_Hr=0;
         int EmpWage = 0;
         int totalEmpWage = 0;
 
-        for (int day=0; day< NUM_OF_WORKING_DAYS;day++) {
+        while (day <= NUM_OF_WORKING_DAYS && EmpHr < MAX_HR_IN_MONTH)
+        {
+
             int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
 
             switch (empcheck) {
@@ -29,10 +34,16 @@ public class EmployeeWageUC {
 
                 default:
                     EmpHr = 0;
+
+
             }
             EmpWage = EmpHr * Emp_RATE_PER_HR;
             totalEmpWage += EmpWage;
-            System.out.println("Employee Wage : " + EmpWage);
+            Total_Emp_Hr += EmpHr;
+
+            System.out.println("Day " + day + " Employee Wage : " + EmpWage + "    Total Working Hr : " + Total_Emp_Hr);
+
+            day++;
         }
         System.out.println("Toatal Emp Wage : " + totalEmpWage);
     }
