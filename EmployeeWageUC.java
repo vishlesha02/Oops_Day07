9b3d5dee5dc33c2c5271288652e2a6cdad4d3134
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-28 11:30:55 +0530

    Add files via upload

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -6,17 +6,24 @@ package Day7;
 
 public class EmployeeWageUC {
     public static void main(String[] args) {
-
-
         int IS_FULL_TIME = 1;
-        double empcheck = Math.floor(Math.random() * 10) % 2;
+        int Emp_RATE_PER_Hr=20;
 
-        if (empcheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
+        int empHrs=0;
+        int empWage=0;
+        double empcheck=Math.floor(Math.random()* 10)% 2;
+
+        if(empcheck== IS_FULL_TIME)
+        {
+            empHrs=8;
         }
+        else
+        {
+            empHrs=0;
 
+        }
+        empWage=empHrs * Emp_RATE_PER_Hr;
+        System.out.println("Employee Wage : " + empWage);
 
     }
 }
