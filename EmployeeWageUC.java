18cfd794f23c1d674201a5e7858c46dcaa85edc1
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-28 13:30:41 +0530

    Add files via upload

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -5,28 +5,35 @@
 package Day7;
 
 public class EmployeeWageUC {
-    public static void main(String[] args) {
-        final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        final int Emp_RATE_PER_HR = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int Emp_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS=20;
 
+    public static void main(String[] args) {
         int EmpHr = 0;
         int EmpWage = 0;
-        int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
+        int totalEmpWage = 0;
 
-        switch (empcheck) {
-            case IS_PART_TIME:
-                EmpHr = 4;
-                break;
+        for (int day=0; day< NUM_OF_WORKING_DAYS;day++) {
+            int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-            case IS_FULL_TIME:
-                EmpHr = 8;
-                break;
+            switch (empcheck) {
+                case IS_PART_TIME:
+                    EmpHr = 4;
+                    break;
 
-            default:
-                EmpHr = 0;
+                case IS_FULL_TIME:
+                    EmpHr = 8;
+                    break;
+
+                default:
+                    EmpHr = 0;
+            }
+            EmpWage = EmpHr * Emp_RATE_PER_HR;
+            totalEmpWage += EmpWage;
+            System.out.println("Employee Wage : " + EmpWage);
         }
-        EmpWage = EmpHr * Emp_RATE_PER_HR;
-        System.out.println("Employee Wage : " + EmpWage);
+        System.out.println("Toatal Emp Wage : " + totalEmpWage);
     }
 }
