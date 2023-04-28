75c4f01bc29a29086b428753dbab3df3f7eab4f4
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-28 11:32:20 +0530

    Add files via upload

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -6,24 +6,28 @@ package Day7;
 
 public class EmployeeWageUC {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
         int Emp_RATE_PER_Hr=20;
 
-        int empHrs=0;
-        int empWage=0;
-        double empcheck=Math.floor(Math.random()* 10)% 2;
+        int EmpWage=0;
+        int EmpHrs=0;
+        double empcheck=Math.floor(Math.random()* 10)% 3;
 
-        if(empcheck== IS_FULL_TIME)
-        {
-            empHrs=8;
-        }
-        else
-        {
-            empHrs=0;
 
+        if(empcheck==IS_PART_TIME)
+        {
+            EmpHrs=4;
         }
-        empWage=empHrs * Emp_RATE_PER_Hr;
-        System.out.println("Employee Wage : " + empWage);
+        else if(empcheck == IS_FULL_TIME)
+        {
+            EmpHrs=8;
+        }
+        else {
+            EmpHrs=0;
+        }
+        EmpWage=EmpHrs*Emp_RATE_PER_Hr;
+        System.out.println("Employee Wage : "+ EmpWage);
 
     }
 }
