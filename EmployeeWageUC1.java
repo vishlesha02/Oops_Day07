97c6cb0023699c29fa9e6f7449c03afa9a217a31
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-28 11:13:52 +0530

    Add files via upload

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,22 @@
+//Check Employee is present or Absent
+
+
+
+package Day7;
+
+public class EmployeeWageUC1 {
+    public static void main(String[] args) {
+
+
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+
+    }
+}
---
+++ EmployeeWageUC2.java
@@ -0,0 +1,25 @@
+package Day7;
+
+public class EmployeeWageUC2 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        int Emp_RATE_PER_Hr=20;
+
+        int empHrs=0;
+        int empWage=0;
+        double empcheck=Math.floor(Math.random()* 10)% 2;
+
+        if(empcheck== IS_FULL_TIME)
+        {
+            empHrs=8;
+        }
+        else
+        {
+           empHrs=0;
+
+        }
+        empWage=empHrs * Emp_RATE_PER_Hr;
+        System.out.println("Employee Wage : " + empWage);
+
+    }
+}
